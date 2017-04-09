541f1cea231d6828b18c45521d988b9bc8fa47b9
acusp <acusp.xu@gmail.com>
2017-04-09 22:03:35 +0800

    Maintain | delete python challenge
    Signed-off-by: acusp <acusp.xu@gmail.com>

--- PythonChallenge/c01.py
+++
@@ -1,8 +0,0 @@
-# http://www.pythonchallenge.com/pc/def/0.html
-# Compute 2 ** 28
-
-base_url = "http://www.pythonchallenge.com/pc/def/%s.html"
-
-print (base_url % 2 ** 38)
-
-# http://www.pythonchallenge.com/pc/def/274877906944.html
--- README.md
+++
@@ -1,3 +0,0 @@
-## PythonChallenge
-
-Record my solutions for the [python challenge](http://www.pythonchallenge.com/)
