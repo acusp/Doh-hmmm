5fb6cc9e8312aa3e7232982c099d63dd1ea5a8f0
acusp <acusp.xu@gmail.com>
2018-02-02 11:05:11 +0800

    Add kivy demo

---
+++ python/kivy/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+TestApp().run()
