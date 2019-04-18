0c88dc383d57eb6de48469adab9cac48a513f2c6
acusp <acusp.xu@gmail.com>
2019-04-18 12:02:09 +0800

    Add offer floder

---
+++ coder/offer/01.js
@@ -0,0 +1,38 @@
+// Solution 1:  from array[0][0] to array[row][col]
+function Find1(target, array)
+{
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (target == array[i][j]) {
+                return true;
+            } else if (target < array[i][j]) {
+                break;
+            }
+        }
+    }
+    return false;
+}
+
+// Solution 2: from array[row][0] -> array[0][x]
+function Find2(target, array) {
+    let row = array.length -1,
+        col = array[0].length -1;
+
+    for (let i = 0; i <= col; i++) {
+        // console.log("row = " + row + ", i = " + i);
+        if (target == array[row][i]) {
+            return true;
+        } else if (target < array[row][i]) {
+            if (0 == row)
+                break;
+            row--;
+            i--;
+        }
+        continue;
+    }
+    return false;
+}
+
+// test
+let arr = [[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]];
+console.log(Find2(5, arr));
