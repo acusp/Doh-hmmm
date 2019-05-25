75a0bcc4d878d2abff6aa57b72022e7bf0a30409
acusp <acusp.xu@gmail.com>
2019-05-25 20:29:21 +0800

    Add(python): tornado http client demo

---
+++ python/tornado/05-http_client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+from tornado import httpclient
+
+http_client = httpclient.HTTPClient()
+try:
+    response = http_client.fetch("https://www.baidu.com")
+    print(response.body)
+except httpclient.HTTPError as e:
+    # HTTPError is raised for non-200 responses; the response
+    # can be found in e.response.
+    print("Error: " + str(e))
+except Exception as e:
+    # Other errors are possible, such as IOError.
+    print("Error: " + str(e))
+http_client.close()
